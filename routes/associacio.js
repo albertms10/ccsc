a83c28e6c314cd3964c9e9ca85c6919713e2690f
albert <albertmasa2@gmail.com>
2020-04-07 18:21:36 +0200

    Consulta d'agrupacions fa servir `nom` en cas que `nom_curt` sigui buit.

--- routes/associacio.js
+++ routes/associacio.js
@@ -13,10 +13,10 @@ module.exports = (app) => {
 
   app.get('/api/associacio/agrupacions', (req, res, next) => {
     connection.query(
-        `SELECT agrupacions.*
+        `SELECT id_agrupacio, a.nom, IFNULL(nom_curt, a.nom) AS nom_curt, descripcio, num_persones
          FROM associacio
-                  INNER JOIN agrupacions_associacio USING (id_associacio)
-                  INNER JOIN agrupacions USING (id_agrupacio);`,
+                  INNER JOIN agrupacions_associacio aa USING (id_associacio)
+                  INNER JOIN agrupacions a USING (id_agrupacio);`,
       (err, rows) => {
         if (err) next(err);
         res.send(rows);
