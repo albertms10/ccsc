e81aa920fae64759e55832e08596e63fb97af72f
albert <albertmasa2@gmail.com>
2020-05-01 00:12:18 +0200

    Afegida restricció d’ordenament dels titulars quan és nul

--- routes/home.js
+++ routes/home.js
@@ -1,11 +1,11 @@
 module.exports = (app) => {
   const connection = app.get("connection");
 
-  app.post("/api/home/titulars", (req, res, next) => {
+  app.get("/api/home/titulars", (req, res, next) => {
     connection.query(
       `SELECT *
          FROM titulars
-         ORDER BY ordre, data_inici;`,
+         ORDER BY ordre IS NULL, data_inici;`,
       (err, rows) => {
         if (err) next(err);
         res.send(rows);
