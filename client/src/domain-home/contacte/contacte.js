882d479681e060634adf6fd1db8d98fabcb6e24e
albert <albertmasa2@gmail.com>
2020-05-01 22:00:26 +0200

    Millorat el formulari de contacte

--- client/src/domain-home/contacte/contacte.js
+++ client/src/domain-home/contacte/contacte.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { Form, Input, Typography } from "antd";
 import { PageSkeleton } from "../../components/page-skeleton";
+import { Container } from "../../standalone/container";
 
 const { Paragraph } = Typography;
+const { TextArea } = Input;
 
 export default () => (
   <PageSkeleton title="Contacte">
@@ -12,42 +14,44 @@ export default () => (
       deixar-nos algun comentari?
     </Paragraph>
     <Paragraph>Endavant! T’estarem agraïts!</Paragraph>
-    <Form layout="vertical">
-      <Form.Item
-        label="Nom"
-        name="nom"
-        rules={[
-          { required: true, message: "Si us plau, introdueixi el seu nom." },
-        ]}
-      >
-        <Input autoFocus />
-      </Form.Item>
-      <Form.Item
-        label="Adreça electrònica"
-        name="email"
-        rules={[
-          {
-            required: true,
-            message:
-              "Si us plau, introdueixi la seva adreça electrònica de contacte.",
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-      <Form.Item
-        label="Missatge"
-        name="missatge"
-        rules={[
-          {
-            required: true,
-            message: "Si us plau, introdueixi el seu missatge.",
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-    </Form>
+    <Container>
+      <Form layout="vertical" size="large">
+        <Form.Item
+          label="Nom"
+          name="nom"
+          rules={[
+            { required: true, message: "Si us plau, introdueixi el seu nom." },
+          ]}
+        >
+          <Input autoFocus />
+        </Form.Item>
+        <Form.Item
+          label="Adreça electrònica"
+          name="email"
+          rules={[
+            {
+              required: true,
+              message:
+                "Si us plau, introdueixi la seva adreça electrònica de contacte.",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Missatge"
+          name="missatge"
+          rules={[
+            {
+              required: true,
+              message: "Si us plau, introdueixi el seu missatge.",
+            },
+          ]}
+        >
+          <TextArea rows={4} />
+        </Form.Item>
+      </Form>
+    </Container>
     <Paragraph>
       Si ho prefereixes, pots contactar amb nosaltres, també, amb el correu
       electrònic{" "}
