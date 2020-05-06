815978fe63b4a50d610dce2bd90cdc270a1187f0
albert <albertmasa2@gmail.com>
2020-05-06 14:34:00 +0200

    Millorat l’estil dels subtítols de cos

--- client/src/domain-tauler/socis/components/perfil-soci/components/soci-tab-general/soci-tab-general.js
+++ client/src/domain-tauler/socis/components/perfil-soci/components/soci-tab-general/soci-tab-general.js
@@ -1,7 +1,8 @@
 import React, { useContext } from "react";
-import { Descriptions } from "antd";
+import { Descriptions, Space } from "antd";
 import moment from "moment";
 import { SociContext } from "../../perfil-soci";
+import { SubHeader } from "../../../../../../standalone/sub-header";
 
 const { Item } = Descriptions;
 
@@ -9,15 +10,21 @@ export default () => {
   const soci = useContext(SociContext);
 
   return (
-    <>
-      <Descriptions title="Dades personals">
-        <Item label="Adreça electrònica">{soci.email}</Item>
-        {soci.telefon ? <Item label="Telèfon">{soci.telefon}</Item> : ""}
-      </Descriptions>
-      <Descriptions title="Dades de contacte">
-        <Item label="DNI">{soci.dni}</Item>
+    <Space size="middle" direction="vertical">
+      <SubHeader title="Dades personals" />
+      <Descriptions size="middle" bordered>
+        <Item label="DNI" span={3}>
+          {soci.dni}
+        </Item>
         <Item label="Naixement">{moment(soci.naixement).format("LL")}</Item>
       </Descriptions>
-    </>
+      <SubHeader title="Dades de contacte" />
+      <Descriptions size="middle" bordered>
+        <Item label="Adreça electrònica">
+          <a href={`mailto:${soci.email}`}>{soci.email}</a>
+        </Item>
+        {soci.telefon ? <Item label="Telèfon">{soci.telefon}</Item> : ""}
+      </Descriptions>
+    </Space>
   );
 };
--- client/src/standalone/sub-header/sub-header.js
+++ client/src/standalone/sub-header/sub-header.js
@@ -1,12 +1,13 @@
 import React from "react";
 
-export default ({ title }) => (
+export default ({ title, style }) => (
   <div
     style={{
       textTransform: "uppercase",
       letterSpacing: ".1rem",
       color: "#777",
       marginTop: "1rem",
+      ...style,
     }}
   >
     {title}
