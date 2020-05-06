db759e21689a96a6b625c6af66254c9e38671f83
albert <albertmasa2@gmail.com>
2020-05-07 00:02:44 +0200

    Millorada la pàgina d’inici amb un títol de benvinguda

--- client/src/domain-tauler/inici/inici.js
+++ client/src/domain-tauler/inici/inici.js
@@ -1,29 +1,41 @@
 import React from "react";
-import { Col, Row } from "antd";
+import { Col, Row, Typography } from "antd";
 import {
   AssajosCountStatistics,
   ConcertsCountStatistics,
   ProjectesCountStatistics,
   SocisCountStatistics,
 } from "../../standalone/statistics";
+import { SafeMargin } from "../../standalone/safe-margin";
+import { useSelector } from "react-redux";
 
-const Inici = () => (
-  <div style={{ margin: 32 }}>
-    <Row type="flex" gutter={[32, 32]}>
-      <Col xs={24} sm={12} md={6} flex={1}>
-        <SocisCountStatistics />
-      </Col>
-      <Col xs={24} sm={12} md={6} flex={1}>
-        <ProjectesCountStatistics />
-      </Col>
-      <Col xs={24} sm={12} md={6} flex={1}>
-        <ConcertsCountStatistics />
-      </Col>
-      <Col xs={24} sm={12} md={6} flex={1}>
-        <AssajosCountStatistics />
-      </Col>
-    </Row>
-  </div>
-);
+const { Title } = Typography;
 
-export default Inici;
+export default () => {
+  const { es_dona } = useSelector((store) => store.user.currentUser);
+
+  return (
+    <SafeMargin>
+      <Row type="flex" gutter={[32, 32]}>
+        <Col span={24}>
+          <Title style={{ color: "#555" }}>
+            Benvingu{es_dona ? "da" : "t"} a l’Associació Musical Catalana
+            Crescendo
+          </Title>
+        </Col>
+        <Col xs={24} sm={12} md={6} flex={1}>
+          <SocisCountStatistics />
+        </Col>
+        <Col xs={24} sm={12} md={6} flex={1}>
+          <ProjectesCountStatistics />
+        </Col>
+        <Col xs={24} sm={12} md={6} flex={1}>
+          <ConcertsCountStatistics />
+        </Col>
+        <Col xs={24} sm={12} md={6} flex={1}>
+          <AssajosCountStatistics />
+        </Col>
+      </Row>
+    </SafeMargin>
+  );
+};
