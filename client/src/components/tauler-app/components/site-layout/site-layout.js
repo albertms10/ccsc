a6e0ce4d1ca242d4f8e89c53094440f1b74405f0
albert <albertmasa2@gmail.com>
2020-05-06 18:31:15 +0200

    Implementació inicial del Header de cada pàgina

--- client/src/components/tauler-app/components/site-layout/site-layout.js
+++ client/src/components/tauler-app/components/site-layout/site-layout.js
@@ -1,11 +1,11 @@
+import React, { useContext, useState } from "react";
 import { Route, Switch } from "react-router-dom";
 import { Inici } from "../../../../domain-tauler/inici";
 import { kebabCase } from "../../../../utils";
 import { Agrupacio } from "../../../../domain-tauler/agrupacio";
 import { Socis } from "../../../../domain-tauler/socis";
 import { PerfilSoci } from "../../../../domain-tauler/socis/components/perfil-soci";
-import { Layout } from "antd";
-import React, { useContext } from "react";
+import { Layout, Typography } from "antd";
 
 import {
   SiderBrokenContext,
@@ -17,8 +17,10 @@ import {
 } from "../../contexts/agrupacions-context";
 
 import "./site-layout.css";
+import { useScrollPosition } from "@n8tb1t/use-scroll-position";
 
 const { Content, Header } = Layout;
+const { Title } = Typography;
 
 export default () => {
   const agrupacions = useContext(AgrupacionsListContext);
@@ -26,15 +28,43 @@ export default () => {
   const collapsed = useContext(SiderCollapsedContext);
   const broken = useContext(SiderBrokenContext);
 
+  const [scrolled, setScrolled] = useState(false);
+  const [pageHeader, setPageHeader] = useState("");
+
+  useScrollPosition(({ _, currPos }) => {
+    if (currPos.y < -15) setScrolled(true);
+    else setScrolled(false);
+  });
+
   return (
     <Layout className="site-layout">
-      <Header className="site-layout-background app-layout-header" />
+      <Header
+        className={
+          "site-layout-background app-layout-header" +
+          (scrolled ? " scrolled" : "")
+        }
+        style={{ marginLeft: broken ? 0 : collapsed ? 80 : 200 }}
+      >
+        <Title
+          className={"app-layout-header-title" + (scrolled ? " scrolled" : "")}
+          level={4}
+        >
+          {pageHeader}
+        </Title>
+      </Header>
       <Content
         className="app-layout-content site-layout-background"
         style={{ marginLeft: broken ? 0 : collapsed ? 80 : 200 }}
       >
         <Switch>
-          <Route exact path="/" component={Inici} />
+          <Route
+            exact
+            path="/"
+            render={() => {
+              setPageHeader("Inici");
+              return <Inici />;
+            }}
+          />
           {loadingAgrupacions
             ? ""
             : agrupacions.map((agrupacio) => (
@@ -42,13 +72,28 @@ export default () => {
                   key={agrupacio.id_agrupacio}
                   exact
                   path={"/" + kebabCase(agrupacio.nom_curt)}
-                  render={(props) => (
-                    <Agrupacio {...props} agrupacio={agrupacio} />
-                  )}
+                  render={(props) => {
+                    setPageHeader(agrupacio.nom);
+                    return <Agrupacio {...props} agrupacio={agrupacio} />;
+                  }}
                 />
               ))}
-          <Route exact path="/socis" component={Socis} />
-          <Route exact path="/socis/:id" component={PerfilSoci} />
+          <Route
+            exact
+            path="/socis"
+            render={() => {
+              setPageHeader("Socis");
+              return <Socis />;
+            }}
+          />
+          <Route
+            exact
+            path="/socis/:id"
+            render={() => {
+              setPageHeader("Soci");
+              return <PerfilSoci />;
+            }}
+          />
         </Switch>
       </Content>
     </Layout>
