a0aa0811f40d57dc01fa59d4c87bc06d63f4fc22
albert <albertmasa2@gmail.com>
2020-05-08 02:13:05 +0200

    Afegit suport per a la prop de component a Authorized

--- client/src/components/authorized/authorized.js
+++ client/src/components/authorized/authorized.js
@@ -9,14 +9,15 @@ import {
  * @param props
  * @param {'juntaDirectiva' | 'directorMusical' | 'admin'} props.authority
  * @param props.render
+ * @param props.component
  * @param props.children
  * @returns {*}
  */
 export default (props) => {
-  const { authority = "juntaDirectiva", render, children } = props;
+  const { authority = "juntaDirectiva", render, component, children } = props;
   const { roles } = useSelector(({ user }) => user.currentUser);
 
-  const returnItem = render ? render(props) : children;
+  const returnItem = render ? render(props) : component ? component : children;
 
   switch (authority) {
     case "juntaDirectiva":
