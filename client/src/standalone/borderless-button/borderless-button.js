027fe88ef76423bb206d0afc1a0bf33fff2ce814
albert <albertmasa2@gmail.com>
2020-05-09 18:39:04 +0200

    Corregit avís de referències al BorderlessButton
    Fixed #9

--- client/src/standalone/borderless-button/borderless-button.js
+++ client/src/standalone/borderless-button/borderless-button.js
@@ -1,16 +1,19 @@
 import { Button, Tooltip } from "antd";
-import React from "react";
+import React, { forwardRef } from "react";
 
-export default ({ tooltip, tooltipPlacement, ...rest }) => (
-  <Tooltip title={tooltip} placement={tooltipPlacement}>
-    <Button
-      {...rest}
-      style={{
-        border: "none",
-        backgroundColor: "transparent",
-        boxShadow: "none",
-        opacity: 0.8,
-      }}
-    />
-  </Tooltip>
-);
+export default forwardRef(({ tooltip, tooltipPlacement, ...rest }, ref) => {
+  return (
+    <Tooltip title={tooltip} placement={tooltipPlacement}>
+      <Button
+        {...rest}
+        ref={ref}
+        style={{
+          border: "none",
+          backgroundColor: "transparent",
+          boxShadow: "none",
+          opacity: 0.8,
+        }}
+      />
+    </Tooltip>
+  );
+});
