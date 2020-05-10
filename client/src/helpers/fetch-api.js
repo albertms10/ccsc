6791614a51b87d20cfb08add70d31a7075024b03
albert <albertmasa2@gmail.com>
2020-05-10 11:31:50 +0200

    Refaccionada la funció que mostra l’avís d’inici de sessió

--- client/src/helpers/fetch-api.js
+++ client/src/helpers/fetch-api.js
@@ -1,6 +1,22 @@
 import { message, Modal } from "antd";
 import { logoutRemoveUser } from "../redux";
 
+const modalWarn = (error, dispatch) => {
+  Modal.warn({
+    title: error.message,
+    content: `Torna a iniciar sessió per comprovar la teva identitat.${
+      error.status ? `Codi d’error: ${error.status}` : ""
+    }`,
+    okText: error.okText ?? "Torna a iniciar sessió",
+    cancelText: "Ignora",
+    okCancel: true,
+    onOk: () => {
+      dispatch(logoutRemoveUser());
+      Modal.destroyAll();
+    },
+  });
+};
+
 /**
  * Fetches the API using the appropriate JWT Access Token.
  * @param {string} url
@@ -23,22 +39,8 @@ export default (url, callback, dispatch, init = {}) => {
       const contentType = res.headers.get("content-type");
       if (contentType && contentType.indexOf("application/json") !== -1)
         res.json().then((data) => {
-          if (data.hasOwnProperty("error")) {
-            Modal.warn({
-              title: data.error.message,
-              content:
-                "Torna a iniciar sessió per comprovar la teva identitat.",
-              okText: "Torna a iniciar sessió",
-              cancelText: "Ignora",
-              okCancel: true,
-              onOk: () => {
-                dispatch(logoutRemoveUser());
-                Modal.destroyAll();
-              },
-            });
-          } else {
-            callback(data);
-          }
+          if (data.hasOwnProperty("error")) modalWarn(data.error, dispatch);
+          else callback(data);
         });
       else if (res.ok) callback();
     })
