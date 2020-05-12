9917feef8e83f6a82b4c9f110358291dbee140b7
albert <albertmasa2@gmail.com>
2020-05-12 02:01:45 +0200

    Afegit mètode d’express GET que retorna l’arxiu index.html a build

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const app = express();
 
@@ -37,6 +38,10 @@ require("./app/routes/projectes.routes")(app);
 require("./app/routes/socis.routes")(app);
 require("./app/routes/titulars.routes")(app);
 require("./app/routes/usuaris.routes")(app);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
 
 const PORT = process.env.PORT || 5000;
 const HOST = "0.0.0.0";
