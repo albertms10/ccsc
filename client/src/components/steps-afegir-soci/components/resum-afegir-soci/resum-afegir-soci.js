84e5a9c471dc235d814e116603a11f6419219ad4
albert <albertmasa2@gmail.com>
2020-05-20 01:11:57 +0200

    Corregida la referència al formulari des de ResumAfegirSoci

--- client/src/components/steps-afegir-soci/components/resum-afegir-soci/resum-afegir-soci.js
+++ client/src/components/steps-afegir-soci/components/resum-afegir-soci/resum-afegir-soci.js
@@ -4,50 +4,73 @@ import { SettingCard } from "../../../../standalone/setting-card";
 import { StatusIcon } from "../../../../standalone/status-icon";
 
 export default ({
-  data,
+  form,
   username,
   loadingUsername,
   acceptaProteccioDades,
   acceptaDretsImatge,
-}) => (
-  <Space direction="vertical">
-    <SettingCard
-      title="Dades personals"
-      info={
-        <Descriptions size="small">
-          <Descriptions.Item label="Nom">{`${data.nom} ${data.cognoms}`}</Descriptions.Item>
-          <Descriptions.Item label="Usuari">
-            {loadingUsername ? <Spin size="small" /> : username}
-          </Descriptions.Item>
-          <Descriptions.Item label="DNI">{data.dni}</Descriptions.Item>
-          <Descriptions.Item label="Naixement" span={2}>
-            {data.naixement && data.naixement.format("LL")}
-          </Descriptions.Item>
-        </Descriptions>
-      }
-    />
-    {data.experiencia_musical || data.estudis_musicals || data.data_alta ? (
+}) => {
+  const data = form.getFieldsValue();
+
+  return (
+    <Space direction="vertical">
       <SettingCard
-        title="Informació musical"
+        title="Dades personals"
         info={
           <Descriptions size="small">
-            {data.experiencia_musical ? (
-              <Descriptions.Item label="Experiència musical" span={3}>
-                {data.experiencia_musical}
-              </Descriptions.Item>
-            ) : (
-              ""
-            )}
-            {data.estudis_musicals ? (
-              <Descriptions.Item label="Estudis musicals" span={3}>
-                {data.estudis_musicals}
-              </Descriptions.Item>
-            ) : (
-              ""
-            )}
-            {data.data_alta ? (
-              <Descriptions.Item label="Data d’alta">
-                {data.data_alta.format("LL")}
+            <Descriptions.Item label="Nom">{`${data.nom} ${data.cognoms}`}</Descriptions.Item>
+            <Descriptions.Item label="Usuari">
+              {loadingUsername ? <Spin size="small" /> : username}
+            </Descriptions.Item>
+            <Descriptions.Item label="DNI">{data.dni}</Descriptions.Item>
+            <Descriptions.Item label="Naixement" span={2}>
+              {data.naixement && data.naixement.format("LL")}
+            </Descriptions.Item>
+          </Descriptions>
+        }
+      />
+      {data.experiencia_musical || data.estudis_musicals || data.data_alta ? (
+        <SettingCard
+          title="Informació musical"
+          info={
+            <Descriptions size="small">
+              {data.experiencia_musical ? (
+                <Descriptions.Item label="Experiència musical" span={3}>
+                  {data.experiencia_musical}
+                </Descriptions.Item>
+              ) : (
+                ""
+              )}
+              {data.estudis_musicals ? (
+                <Descriptions.Item label="Estudis musicals" span={3}>
+                  {data.estudis_musicals}
+                </Descriptions.Item>
+              ) : (
+                ""
+              )}
+              {data.data_alta ? (
+                <Descriptions.Item label="Data d’alta">
+                  {data.data_alta.format("LL")}
+                </Descriptions.Item>
+              ) : (
+                ""
+              )}
+            </Descriptions>
+          }
+        />
+      ) : (
+        ""
+      )}
+      <SettingCard
+        title="Dades de contacte"
+        info={
+          <Descriptions size="small">
+            <Descriptions.Item label="Adreça electrònica" span={2}>
+              {data.email}
+            </Descriptions.Item>
+            {data.telefon ? (
+              <Descriptions.Item label="Telèfon">
+                {data.telefon}
               </Descriptions.Item>
             ) : (
               ""
@@ -55,44 +78,25 @@ export default ({
           </Descriptions>
         }
       />
-    ) : (
-      ""
-    )}
-    <SettingCard
-      title="Dades de contacte"
-      info={
-        <Descriptions size="small">
-          <Descriptions.Item label="Adreça electrònica" span={2}>
-            {data.email}
-          </Descriptions.Item>
-          {data.telefon ? (
-            <Descriptions.Item label="Telèfon">
-              {data.telefon}
+      <SettingCard
+        title="Acceptacions"
+        info={
+          <Descriptions size="small">
+            <Descriptions.Item label="Protecció de dades">
+              <StatusIcon
+                statusId={acceptaProteccioDades ? 1 : 3}
+                label={acceptaProteccioDades ? "Accepto" : "No accepto"}
+              />
             </Descriptions.Item>
-          ) : (
-            ""
-          )}
-        </Descriptions>
-      }
-    />
-    <SettingCard
-      title="Acceptacions"
-      info={
-        <Descriptions size="small">
-          <Descriptions.Item label="Protecció de dades">
-            <StatusIcon
-              statusId={acceptaProteccioDades ? 1 : 3}
-              label={acceptaProteccioDades ? "Accepto" : "No accepto"}
-            />
-          </Descriptions.Item>
-          <Descriptions.Item label="Drets d’imatge">
-            <StatusIcon
-              statusId={acceptaDretsImatge ? 1 : 3}
-              label={acceptaDretsImatge ? "Accepto" : "No accepto"}
-            />
-          </Descriptions.Item>
-        </Descriptions>
-      }
-    />
-  </Space>
-);
+            <Descriptions.Item label="Drets d’imatge">
+              <StatusIcon
+                statusId={acceptaDretsImatge ? 1 : 3}
+                label={acceptaDretsImatge ? "Accepto" : "No accepto"}
+              />
+            </Descriptions.Item>
+          </Descriptions>
+        }
+      />
+    </Space>
+  );
+};
