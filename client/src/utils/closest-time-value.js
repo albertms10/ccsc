ca612fe246fca05292902f7dbf3f01858983ede4
albert <albertmasa2@gmail.com>
2020-05-19 14:32:44 +0200

    Corregit el càlcul de dies a closest-time-value.js

--- client/src/utils/closest-time-value.js
+++ client/src/utils/closest-time-value.js
@@ -1,10 +1,13 @@
+/** @typedef {('s'|'min'|'h'|'d'|'m'|'y')} TimeUnit */
+
 /**
  * Given an integer, finds the propper unit in order to express its value.
  * @param {number} value
- * @param {('s'|'min'|'h'|'d'|'m'|'y')} unit
+ * @param {TimeUnit} unit
  * @returns {string}
  */
 export default (value, unit = "s") => {
+  /** @type {TimeUnit[]} */
   const units = ["s", "min", "h", "d", "m", "y"];
   const durations = [60, 60, 24, 30, 12, 10];
 
@@ -14,13 +17,11 @@ export default (value, unit = "s") => {
   for (
     i = units.indexOf(unit);
     value >= durations[i] && i < durations.length - 1;
-    i++
-  ) {
-    num /= durations[i];
-  }
+    value /= durations[i], num /= durations[i], i++
+  ) {}
 
   num = Math.round(num);
-  return `${num} ${
+  return `${num}\u00a0${
     num === 1
       ? ["segon", "minut", "hora", "dia", "mes", "any"][i]
       : ["segons", "minuts", "hores", "dies", "mesos", "anys"][i]
