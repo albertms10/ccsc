ff798a71787883f4d929e56b0ff3cafc8df6f497
albert <albertmasa2@gmail.com>
2020-05-26 21:12:25 +0200

    Corregida la relació amb l’associació de la consulta d’historial de socis

--- server/controllers/socis.controller.js
+++ server/controllers/socis.controller.js
@@ -43,8 +43,7 @@ exports.socis_historial = (req, res, next) => {
       `SELECT CONCAT('T', num, ' (', REPLACE(id_curs, '-', '–'), ')') AS x, COUNT(*) AS y
          FROM socis
                   INNER JOIN historial_socis hs ON socis.id_soci = hs.id_historial_soci
-                  CROSS JOIN associacio
-                  INNER JOIN cursos USING (id_associacio)
+                  CROSS JOIN cursos
                   INNER JOIN trimestres t USING (id_curs)
          WHERE hs.data_alta <= IFNULL(t.data_final, NOW())
          GROUP BY id_trimestre, t.data_inici
