f34e2e82dafb16481f742b21ff44695006935f21
albert <albertmasa2@gmail.com>
2020-05-27 01:00:38 +0200

    Afegida la llista d’agrupacions a la consulta d’assajos

--- server/query-helpers/assajos.query-helper.js
+++ server/query-helpers/assajos.query-helper.js
@@ -17,14 +17,26 @@ exports.assajos_query_helper =
        WHERE id_estat_confirmacio = (SELECT id_estat_localitzacio)
    )                                                     AS estat_localitzacio,
    (
-       SELECT JSON_ARRAYAGG(
+       SELECT IFNULL(JSON_ARRAYAGG(
+                      JSON_OBJECT(
+                              'id_agrupacio', id_agrupacio,
+                              'nom', nom,
+                              'nom_curt', nom_curt
+                          )
+                  ), '[]')
+       FROM agrupacions
+                INNER JOIN assajos_agrupacions USING (id_agrupacio)
+       WHERE id_assaig = (SELECT a.id_assaig)
+   )                                                     AS agrupacions,
+   (
+       SELECT IFNULL(JSON_ARRAYAGG(
                       JSON_OBJECT(
                               'id_projecte', id_projecte,
                               'titol', titol,
                               'inicials', inicials,
                               'color', color
                           )
-                  )
+                  ), '[]')
        FROM projectes
                 INNER JOIN assajos_projectes USING (id_projecte)
        WHERE id_assaig = (SELECT a.id_assaig)
