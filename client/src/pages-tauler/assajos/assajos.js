cc078a9aa43ad6642c05c56a4cd3c21c7b3f625e
albert <albertmasa2@gmail.com>
2020-05-27 01:01:15 +0200

    Millorada la informació que apareix sobre els assajos

--- client/src/pages-tauler/assajos/assajos.js
+++ client/src/pages-tauler/assajos/assajos.js
@@ -1,19 +1,26 @@
 import { PageHeader } from "antd";
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import { SetPageHeaderContext } from "../../components/tauler-app/components/site-layout/site-layout";
 import { Container } from "../../standalone/container";
 import { LlistaAssajos } from "./components/llista-assajos";
 import { ModalAfegirAssaig } from "./components/modal-afegir-assaig";
 
-export default () => (
-  <>
-    <PageHeader
-      className="main-page-header"
-      ghost={false}
-      title="Assajos"
-      extra={<ModalAfegirAssaig />}
-    />
-    <Container>
-      <LlistaAssajos />
-    </Container>
-  </>
-);
+export default () => {
+  const setPageHeader = useContext(SetPageHeaderContext);
+
+  useEffect(() => setPageHeader("Assajos"), [setPageHeader]);
+
+  return (
+    <>
+      <PageHeader
+        className="main-page-header"
+        ghost={false}
+        title="Assajos"
+        extra={<ModalAfegirAssaig />}
+      />
+      <Container>
+        <LlistaAssajos />
+      </Container>
+    </>
+  );
+};
--- client/src/pages-tauler/assajos/components/llista-assajos/llista-assajos.js
+++ client/src/pages-tauler/assajos/components/llista-assajos/llista-assajos.js
@@ -1,9 +1,14 @@
-import { List } from "antd";
+import { List, Space, Typography } from "antd";
 import moment from "moment";
 import React from "react";
+import { IconAgrupacio } from "../../../../assets/icons";
+import { Authorized } from "../../../../components/authorized";
+import { DropdownBorderlessButton } from "../../../../standalone/dropdown-borderless-button";
+import { FixedTag } from "../../../../standalone/fixed-tag";
 import { useAssajosSoci } from "./hooks";
 
 const { Item } = List;
+const { Text } = Typography;
 
 export default () => {
   const [assajos, loading] = useAssajosSoci();
@@ -13,7 +18,49 @@ export default () => {
       dataSource={assajos}
       loading={loading}
       renderItem={(assaig) => (
-        <Item key={assaig.id_assaig}>
+        <Item
+          key={assaig.id_assaig}
+          actions={[
+            ...(assaig.agrupacions && assaig.agrupacions.length > 0
+              ? [
+                  <Space>
+                    {assaig.agrupacions.map((agrupacio) => (
+                      <IconAgrupacio
+                        key={agrupacio.id_agrupacio}
+                        name={agrupacio.nom_curt}
+                      />
+                    ))}
+                  </Space>,
+                ]
+              : []),
+            ...(assaig.projectes && assaig.projectes.length > 0
+              ? [
+                  <div>
+                    {assaig.projectes.map((projecte) => (
+                      <FixedTag
+                        key={projecte.id_projecte}
+                        childKey={projecte.id_projecte}
+                        tooltip={projecte.titol}
+                        color={"#" + projecte.color}
+                      >
+                        {projecte.inicials}
+                      </FixedTag>
+                    ))}
+                  </div>,
+                ]
+              : []),
+            <Authorized>
+              <DropdownBorderlessButton
+                items={[
+                  {
+                    key: "eliminar",
+                    action: <Text type="danger">Eliminar</Text>,
+                  },
+                ]}
+              />
+            </Authorized>,
+          ]}
+        >
           <Item.Meta
             title={`Assaig${assaig.es_general ? " general" : ""}${
               assaig.es_extra ? " extra" : ""
