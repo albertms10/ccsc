8fd8261d3e0c1865c9a9191ca87415a2f66fdd07
albert <albertmasa2@gmail.com>
2020-05-27 17:42:48 +0200

    Refaccionada la funció helper parseAndSendJSON

--- server/helpers/parse-and-send-json.js
+++ server/helpers/parse-and-send-json.js
@@ -1,16 +1,29 @@
-const parseAndSendJSON = (array, properties, res, next) => {
-  try {
-    array.forEach((element) =>
-      properties.forEach(
-        (property) => (element[property] = JSON.parse(element[property]))
-      )
+/**
+ * Parses given properties of a data object
+ * or array and sends a JSON response.
+ * @param {Object | Array} data
+ * @param {string[]} properties
+ * @param res
+ * @param next
+ */
+const parseAndSendJSON = (data, properties, res, next) => {
+  const parseJSON = (properties, element) =>
+    properties.forEach(
+      (property) => (element[property] = JSON.parse(element[property]))
     );
+
+  const typeOf = (data) => Object.prototype.toString.call(data);
+
+  try {
+    if (typeOf(data) === "[object Array]")
+      data.forEach((element) => parseJSON(properties, element));
+    else if (typeOf(data) === "[object Object]") parseJSON(properties, data);
   } catch (e) {
     next(e);
     return res.end();
   }
 
-  res.json(array);
+  res.json(data);
 };
 
 module.exports = parseAndSendJSON;
