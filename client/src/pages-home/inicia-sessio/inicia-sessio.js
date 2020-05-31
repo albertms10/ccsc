714b81910e3a386c4c49adcdef8d81b2ef3ab6dc
albert <albertmasa2@gmail.com>
2020-05-31 16:40:42 +0200

    Corregit error de picatge

--- client/src/pages-home/inicia-sessio/inicia-sessio.js
+++ client/src/pages-home/inicia-sessio/inicia-sessio.js
@@ -109,7 +109,7 @@ export default () => {
           </Spin>
           <Divider />
           <Form.Item style={{ marginBottom: 0 }}>
-            <Paragraph>Encara no t’has donat d’alt?</Paragraph>
+            <Paragraph>Encara no t’has donat d’alta?</Paragraph>
             <Link to="/donar-alta">
               <Button type="secondary" className="signin-form-button">
                 Donar-se d’alta
