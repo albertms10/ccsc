ed6d3564f273911c343ba347a403f9c95a5465e1
albert <albertmasa2@gmail.com>
2020-06-09 00:04:18 +0200

    feat: Millorada la funció initials

--- client/src/utils/initials.js
+++ client/src/utils/initials.js
@@ -1,14 +1,36 @@
 /**
+ * @typedef {Object} InitialsOptions
+ * @property {number} [minValue=0]
+ * @property {number} [maxInitials=0]
+ */
+
+/**
  * Returns the initial characters of each word in a string.
  * @param {string} s
+ * @param {InitialsOptions} [options]
  * @returns {string}
  */
-export default (s) =>
-  s
-    ? s
-        .trim()
-        .split(" ")
-        .map((w) => w.charAt(0))
-        .join("")
-        .toUpperCase()
-    : "";
+export default (s, options = { minValue: 0, maxInitials: 0 }) => {
+  if (typeof s !== "string" || !s) return "";
+
+  const words = s
+    .trim()
+    .split(" ")
+    .filter((w) => w.length >= options.minValue);
+
+  let initials = [];
+  if (options.maxInitials > 0) {
+    const pushed = [];
+    const sorted = words.concat().sort((a, b) => b.length - a.length);
+
+    for (let i = 0; i < options.maxInitials; i++)
+      pushed.push(words.find((w) => w === sorted[i]));
+    initials = words.filter((w) => pushed.includes(w));
+  }
+
+  if (initials.length === 0) initials = words;
+  return initials
+    .map((w) => (w.length >= options.minValue ? w.charAt(0) : ""))
+    .join("")
+    .toUpperCase();
+};
