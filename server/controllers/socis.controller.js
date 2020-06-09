a4dbdb9339b31dded47ccb2d7da47142f793243b
albert <albertmasa2@gmail.com>
2020-06-09 23:47:33 +0200

    fix: Corregeix consulta socis_get
    Modificats paràmetres d’ordre i de límit en cas que la subconsulta retorni més d’un resultat

--- server/controllers/socis.controller.js
+++ server/controllers/socis.controller.js
@@ -87,23 +87,28 @@ exports.socis_get = (req, res, next) => {
                 email,
                 telefon,
                 IF(
-                        (
-                            SELECT id_soci
-                            FROM socis
-                                     INNER JOIN historial_socis hs ON socis.id_soci = hs.id_historial_soci
-                            WHERE id_soci = (SELECT id_persona)
-                              AND CURRENT_DATE BETWEEN data_alta AND IFNULL(data_baixa, CURRENT_DATE)
-                        ) IS NULL, CAST(FALSE AS JSON), CAST(TRUE AS JSON)
+                        EXISTS(
+                                SELECT id_soci
+                                FROM socis
+                                         INNER JOIN historial_socis hs ON socis.id_soci = hs.id_historial_soci
+                                WHERE id_soci = (SELECT id_persona)
+                                  AND CURRENT_DATE BETWEEN data_alta AND IFNULL(data_baixa, CURRENT_DATE)
+                                ORDER BY data_alta DESC
+                            ) IS NULL, CAST(FALSE AS JSON), CAST(TRUE AS JSON)
                     ) AS estat_actiu,
                 (
                     SELECT MAX(data_alta)
                     FROM historial_socis
                     WHERE id_historial_soci = (SELECT id_persona)
+                    ORDER BY data_alta DESC
+                    LIMIT 1
                 )     AS data_actiu,
                 (
                     SELECT MAX(data_baixa)
                     FROM historial_socis
                     WHERE id_historial_soci = (SELECT id_persona)
+                    ORDER BY data_alta DESC
+                    LIMIT 1
                 )     AS data_inactiu,
                 (
                     SELECT IFNULL(DATEDIFF(data_baixa, data_alta), DATEDIFF(NOW(), data_alta)) + 1
