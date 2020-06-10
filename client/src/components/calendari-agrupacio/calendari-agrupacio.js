265b7687b3b20c939df49a68e6bf6c362f6fb6c9
albert <albertmasa2@gmail.com>
2020-06-10 17:03:04 +0200

    refactor: Reubica la funció dateCellRender

--- client/src/components/calendari-agrupacio/calendari-agrupacio.js
+++ client/src/components/calendari-agrupacio/calendari-agrupacio.js
@@ -1,11 +1,12 @@
 import { LeftOutlined, RightOutlined } from "@ant-design/icons";
 import { Button, Calendar, Col, Row, Space } from "antd";
 import moment from "moment";
-import React, { useCallback, useContext } from "react";
+import React, { useContext } from "react";
 import { AgrupacioContext } from "../../pages-tauler/agrupacio/agrupacio";
 import { BorderlessButton } from "../../standalone/borderless-button";
 import { Container } from "../../standalone/container";
 import { SearchComplete } from "../../standalone/search-complete";
+import { eventSearchFilter } from "../../utils";
 import "./calendari-agrupacio.css";
 import { CalendariAgrupacioCell } from "./components/calendari-agrupacio-cell";
 import { CalendariResultLabel } from "./components/calendari-result-label";
@@ -15,25 +16,18 @@ export default () => {
   const { id_agrupacio } = useContext(AgrupacioContext);
   const [esdeveniments] = useCalendariEsdeveniments(id_agrupacio);
 
-  const cellRender = useCallback(
-    (currentDay) => {
-      const esdevenimentsActuals = esdeveniments.filter(({ dia_inici }) =>
-        currentDay.isSame(dia_inici, "day")
-      );
-
-      return (
-        <CalendariAgrupacioCell esdevenimentsActuals={esdevenimentsActuals} />
-      );
-    },
-    [esdeveniments]
-  );
-
   return (
     <Container reducedPadding>
       <Calendar
         className="calendari-agrupacio"
         style={{ margin: "1rem" }}
-        dateCellRender={cellRender}
+        dateCellRender={(currentDay) => (
+          <CalendariAgrupacioCell
+            esdevenimentsActuals={esdeveniments.filter(({ dia_inici }) =>
+              currentDay.isSame(dia_inici, "day")
+            )}
+          />
+        )}
         headerRender={({ value, onChange }) => (
           <div className="ant-picker-calendar-header">
             <Row align="middle" gutter={16}>
@@ -71,20 +65,9 @@ export default () => {
                 <SearchComplete
                   data={esdeveniments}
                   onSelect={(value, option) => onChange(moment(option.date))}
-                  filter={(value, option) => {
-                    const dia = moment(option.data_inici);
-                    return value
-                      .split(" ")
-                      .every(
-                        (frag) =>
-                          option.titol
-                            .toLowerCase()
-                            .includes(frag.toLowerCase()) ||
-                          dia.format("L").includes(frag) ||
-                          dia.format("LT").includes(frag) ||
-                          dia.format("LL").includes(frag)
-                      );
-                  }}
+                  filter={(value, option) =>
+                    eventSearchFilter(value, option.titol, [option.data_inici])
+                  }
                   optionRenderObject={(esdeveniment) => ({
                     key: esdeveniment.id_esdeveniment,
                     value: (
