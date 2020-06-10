61995bc1375c34676b375791410d255cad200936
albert <albertmasa2@gmail.com>
2020-06-10 18:55:43 +0200

    feat: Afegeix cerca a la llista de projectes

--- client/src/pages-tauler/projectes/components/llista-projectes/llista-projectes.js
+++ client/src/pages-tauler/projectes/components/llista-projectes/llista-projectes.js
@@ -1,28 +1,52 @@
-import { List, Space, Typography } from "antd";
-import React, { useContext } from "react";
+import { Input, List, Space, Typography } from "antd";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { IconAgrupacio } from "../../../../assets/icons";
 import { Authorized } from "../../../../components/authorized";
 import { AgrupacionsListContext } from "../../../../components/tauler-app/contexts/agrupacions-context";
 import { ColorCard } from "../../../../standalone/color-card";
 import { DropdownBorderlessButton } from "../../../../standalone/dropdown-borderless-button";
-import { literalList } from "../../../../utils";
+import { eventSearchFilter, literalList } from "../../../../utils";
 import { useEliminarProjecte, useProjectes } from "./hooks";
 import "./llista-projectes.css";
 
 const { Item } = List;
 const { Text } = Typography;
+const { Search } = Input;
 
 export default () => {
   const agrupacions = useContext(AgrupacionsListContext);
+
+  const [searchValue, setSearchValue] = useState("");
+
   const [projectes, loading] = useProjectes();
   const [showDeleteConfirm] = useEliminarProjecte();
 
   return (
     <div className="llista-projectes">
+      <Search
+        placeholder="Cerca assajos"
+        size="large"
+        value={searchValue}
+        onChange={({ target }) => setSearchValue(target.value.toLowerCase())}
+        style={{ width: "100%", marginBottom: "1rem" }}
+      />
       <List
         loading={loading}
-        dataSource={projectes}
+        dataSource={
+          searchValue.length > 0
+            ? projectes.filter((projecte) =>
+                eventSearchFilter(searchValue, {
+                  texts: [
+                    projecte.titol,
+                    projecte.descripcio,
+                    projecte.any_inici_curs,
+                    projecte.any_final_curs,
+                  ],
+                })
+              )
+            : projectes
+        }
         renderItem={(projecte) => (
           <Item
             key={projectes.id_projecte}
