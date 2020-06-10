ecb44662504e7f0cc25c2b9ab2e192f8f0efcd01
albert <albertmasa2@gmail.com>
2020-06-10 18:56:59 +0200

    refactor: Afegeix camps del curs a la consulta de projectes

--- server/controllers/socis.controller.js
+++ server/controllers/socis.controller.js
@@ -345,7 +345,11 @@ exports.socis_detall_projectes = (req, res, next) => {
                          descripcio,
                          inicials,
                          color,
+                         data_inici,
+                         data_final,
                          id_curs,
+                         YEAR(cursos.inici) AS any_inici_curs,
+                         YEAR(cursos.final) AS any_final_curs,
                          (
                              SELECT IFNULL(JSON_ARRAYAGG(
                                                    JSON_OBJECT(
@@ -356,7 +360,7 @@ exports.socis_detall_projectes = (req, res, next) => {
                              FROM directors_projectes
                                       INNER JOIN persones p ON directors_projectes.id_director = p.id_persona
                              WHERE id_projecte = (SELECT projectes.id_projecte)
-                         ) AS directors,
+                         )                  AS directors,
                          (
                              SELECT IFNULL(JSON_ARRAYAGG(
                                                    JSON_OBJECT(
@@ -368,10 +372,11 @@ exports.socis_detall_projectes = (req, res, next) => {
                              FROM projectes_agrupacions
                                       INNER JOIN agrupacions USING (id_agrupacio)
                              WHERE id_projecte = (SELECT projectes.id_projecte)
-                         ) AS agrupacions
+                         )                  AS agrupacions
          FROM projectes
                   INNER JOIN projectes_agrupacions USING (id_projecte)
                   INNER JOIN socis_agrupacions USING (id_agrupacio)
+                  INNER JOIN cursos USING (id_curs)
          WHERE id_soci = @id_soci
             OR EXISTS(
                  SELECT *
