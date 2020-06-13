eede65496e2b9c20bec9f7d2211161fa72110919
albert <albertmasa2@gmail.com>
2020-06-13 23:34:46 +0200

    fix: Inclou el canvi de capçalera de Projectes

--- client/src/pages-tauler/projectes/projectes.js
+++ client/src/pages-tauler/projectes/projectes.js
@@ -1,11 +1,16 @@
 import { PageHeader } from "antd";
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import { Authorized } from "../../components/authorized";
+import { SetPageHeaderContext } from "../../components/tauler-app/components/site-layout/site-layout";
 import { Container } from "../../standalone/container";
 import { LlistaProjectes } from "./components/llista-projectes";
 import { ModalAfegirProjecte } from "./components/modal-afegir-projecte";
 
 export default () => {
+  const setPageHeader = useContext(SetPageHeaderContext);
+
+  useEffect(() => setPageHeader("Projectes"), [setPageHeader]);
+
   return (
     <>
       <PageHeader
