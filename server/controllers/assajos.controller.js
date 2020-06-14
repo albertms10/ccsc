98ddaeced15ad6d68289e9bc377db1db9594b2f8
albert <albertmasa2@gmail.com>
2020-06-14 22:33:04 +0200

    fix: Modifica la consulta de veus convocades a assaig

--- server/controllers/assajos.controller.js
+++ server/controllers/assajos.controller.js
@@ -235,15 +235,18 @@ exports.assajos_detall_veus_get = (req, res, next) => {
 
   pool
     .query(
-        `SELECT *
-         FROM veus
-         WHERE id_veu NOT IN (
-             SELECT id_veu
-             FROM veus
-                      INNER JOIN veus_convocades_assaig USING (id_veu)
-             WHERE ?
-         );`,
-      { id_assaig }
+        `SELECT *,
+                IF(
+                        EXISTS(
+                                SELECT id_veu
+                                FROM veus
+                                         INNER JOIN veus_convocades_assaig USING (id_veu)
+                                WHERE id_assaig = ?
+                                  AND id_veu = (SELECT v.id_veu)
+                            ), CAST(TRUE AS JSON), CAST(FALSE AS JSON)
+                    ) AS convocada
+         FROM veus v;`,
+      [id_assaig]
     )
     .then((veus) => res.json(veus))
     .catch((e) => next(e));
