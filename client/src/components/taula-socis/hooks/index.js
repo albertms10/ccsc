ec23ee8d1013291572315c74487ddbaa334a4618
albert <albertmasa2@gmail.com>
2020-06-15 23:19:34 +0200

    refactor: Subsituït hook useSearchSocis per la funció eventSearchFilter

--- client/src/components/taula-socis/hooks/index.js
+++ client/src/components/taula-socis/hooks/index.js
@@ -1,2 +1,1 @@
-export { default as useSearchSocis } from "./use-search-socis";
 export { default as useEliminarSoci } from "./use-eliminar-soci";
--- client/src/components/taula-socis/taula-socis.js
+++ client/src/components/taula-socis/taula-socis.js
@@ -1,10 +1,10 @@
 import { Grid, Input, Table, Typography } from "antd";
 import PropTypes from "prop-types";
-import React from "react";
+import React, { useState } from "react";
 import { SociPropTypes } from "../../typedef/prop-types";
+import { eventSearchFilter } from "../../utils";
 import { CellNomSoci } from "./components/cell-nom-soci";
 import { DropdownRowSoci } from "./components/dropdown-row-soci";
-import { useSearchSocis } from "./hooks";
 import "./taula-socis.css";
 
 const { Paragraph } = Typography;
@@ -12,7 +12,7 @@ const { Search } = Input;
 
 const TaulaSocis = ({ socis, loading }) => {
   const breakpoint = Grid.useBreakpoint();
-  const [searchValue, setSearchValue, filteredSocis] = useSearchSocis(socis);
+  const [searchValue, setSearchValue] = useState("");
 
   const columns = [
     {
@@ -74,7 +74,20 @@ const TaulaSocis = ({ socis, loading }) => {
         style={{ width: "100%", marginBottom: "1rem" }}
       />
       <Table
-        dataSource={searchValue ? filteredSocis : socis}
+        dataSource={
+          searchValue.length > 0
+            ? socis.filter((soci) =>
+                eventSearchFilter(searchValue, {
+                  texts: [
+                    soci.nom_complet,
+                    soci.username,
+                    soci.email,
+                    soci.telefon,
+                  ],
+                })
+              )
+            : socis
+        }
         rowKey="id_persona"
         loading={loading}
         pagination={{ hideOnSinglePage: true, responsive: true }}
