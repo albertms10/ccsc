dbd90244ffdbdea6703636eed8cacb1775df165b
albert <albertmasa2@gmail.com>
2020-06-16 13:13:51 +0200

    feat: Incorpora més dades a la consulta de propers_assajos_socis

--- server/queries/socis/select__propers_assajos_soci.sql
+++ server/queries/socis/select__propers_assajos_soci.sql
@@ -1,21 +1,84 @@
 SET @id_soci = ?;
 
-SELECT *,
+SELECT a.*,
+       IFNULL(CONCAT(dia_inici, ' ', hora_inici), dia_inici)                    AS data_inici,
+       DATE_FORMAT(dia_inici, '%Y-%m-%d')                                       AS dia_inici,
+       hora_inici,
+       IFNULL(CONCAT(IFNULL(dia_final, dia_inici), ' ', hora_final), dia_final) AS data_final,
+       IFNULL(DATE_FORMAT(dia_final, '%Y-%m-%d'), dia_inici)                    AS dia_final,
+       hora_final,
+       CONCAT(
+               'Assaig',
+               IF(es_general, ' general', ''),
+               IF(es_extra, ' extra', '')
+           )                                                                    AS titol,
+       (
+           SELECT estat
+           FROM estats_confirmacio
+           WHERE id_estat_confirmacio = (SELECT id_estat_esdeveniment)
+       )                                                                        AS estat_esdeveniment,
+       (
+           SELECT estat
+           FROM estats_confirmacio
+           WHERE id_estat_confirmacio = (SELECT id_estat_localitzacio)
+       )                                                                        AS estat_localitzacio,
+       (
+           SELECT CONCAT_WS(' ', tv.nom, CONCAT(carrer, ','),
+                            CONCAT(IFNULL(CONCAT(numero, '–', fins_numero), CONCAT(numero)), ','), c.nom,
+                            CONCAT('(',
+                                   (SELECT nom FROM ciutats WHERE id_ciutat = (SELECT c.id_provincia)),
+                                   ')'))
+           FROM localitzacions
+                    INNER JOIN tipus_vies tv USING (id_tipus_via)
+                    INNER JOIN ciutats c USING (id_ciutat)
+           WHERE id_localitzacio = (SELECT a.id_localitzacio)
+       )                                                     AS localitzacio,
+       (
+           SELECT e.nom
+           FROM localitzacions
+                    INNER JOIN establiments e ON localitzacions.id_localitzacio = e.id_establiment
+           WHERE id_localitzacio = (SELECT a.id_localitzacio)
+       )                                                     AS establiment,
+       (
+           SELECT IFNULL(JSON_ARRAYAGG(
+                                 JSON_OBJECT(
+                                         'id_formacio', id_formacio,
+                                         'nom', nom,
+                                         'nom_curt', nom_curt
+                                     )
+                             ), '[]')
+           FROM formacions
+                    INNER JOIN assajos_formacions USING (id_formacio)
+           WHERE id_assaig = (SELECT a.id_assaig)
+       )                                                                        AS formacions,
+       (
+           SELECT IFNULL(JSON_ARRAYAGG(
+                                 JSON_OBJECT(
+                                         'id_projecte', id_projecte,
+                                         'titol', titol,
+                                         'inicials', inicials,
+                                         'color', color
+                                     )
+                             ), '[]')
+           FROM projectes
+                    INNER JOIN assajos_projectes USING (id_projecte)
+           WHERE id_assaig = (SELECT a.id_assaig)
+       )                                                                        AS projectes,
        (
            SELECT nom
            FROM veus
            WHERE veus.id_veu = (SELECT a.id_veu)
-       ) AS nom_veu,
+       )                                                                        AS nom_veu,
        (
            SELECT abreviatura
            FROM veus
            WHERE veus.id_veu = (SELECT a.id_veu)
-       ) AS abreviatura_veu,
+       )                                                                        AS abreviatura_veu,
        (
            SELECT estat
            FROM estats_confirmacio
            WHERE id_estat_confirmacio = (SELECT a.id_estat_confirmacio)
-       ) AS estat_confirmacio
+       )                                                                        AS estat_confirmacio
 FROM (
          SELECT DISTINCT *,
                          IFNULL((
