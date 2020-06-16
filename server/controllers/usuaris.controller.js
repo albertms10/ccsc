002cc41ec36744692293697c432bb46ae6636fea
albert <albertmasa2@gmail.com>
2020-06-16 11:07:05 +0200

    refactor: Organitza les consultes d’usuaris en fitxers SQL independents

--- server/controllers/usuaris.controller.js
+++ server/controllers/usuaris.controller.js
@@ -1,19 +1,12 @@
+const { queryFile } = require("../helpers");
+
 exports.usuaris_detall_firstavailablenum = (req, res, next) => {
   const pool = req.app.get("pool");
   const username = req.params.username;
 
   pool
-    .query(
-        `SELECT MAX(
-                        CAST(
-                                SUBSTR(username, LOCATE(?, username) + LENGTH(?)) AS UNSIGNED
-                            )
-                    ) + 1 AS first_available_num
-         FROM usuaris
-         WHERE LOCATE(?, username) = 1;`,
-      [username, username, username]
-    )
-    .then(([{ first_available_num }]) =>
+    .query(queryFile("usuaris/select__first_available_num_usuari"), [username])
+    .then(([_, [{ first_available_num }]]) =>
       res.json(parseInt(first_available_num || 0))
     )
     .catch((e) => next(e));
---
+++ server/queries/usuaris/select__first_available_num_usuari.sql
@@ -0,0 +1,9 @@
+SET @username = ?;
+
+SELECT MAX(
+               CAST(
+                       SUBSTR(username, LOCATE(@username, username) + LENGTH(@username)) AS UNSIGNED
+                   )
+           ) + 1 AS first_available_num
+FROM usuaris
+WHERE LOCATE(@username, username) = 1;
