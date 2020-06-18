34dc951587ece04a804e76ba025d600bf137f28b
albert <albertmasa2@gmail.com>
2020-06-18 12:31:00 +0200

    feat: Millora la informació de ContentListMoviments

--- client/src/pages-tauler/detall-obra/components/content-list-moviments/content-list-moviments.js
+++ client/src/pages-tauler/detall-obra/components/content-list-moviments/content-list-moviments.js
@@ -1,11 +1,13 @@
+import { Typography } from "antd";
 import React, { useContext } from "react";
 import { ContentList } from "../../../../standalone/content-list";
+import { timeDuration } from "../../../../utils";
 import { ObraContext } from "../../detall-obra";
 import { ModalAfegirMoviment } from "../modal-afegir-moviment";
 import { useMoviments } from "./hooks";
 
 export default () => {
-  const { id_obra } = useContext(ObraContext);
+  const { id_obra, durada_total } = useContext(ObraContext);
 
   const [moviments, loadingMoviments, getMoviments] = useMoviments(id_obra);
 
@@ -17,9 +19,15 @@ export default () => {
         id: moviment.id_moviment,
         title: moviment.titol,
         link: `/obres/${moviment.id_obra}/moviments/${moviment.id_moviment}`,
-        extra: moviment.durada || "Sense durada",
+        extra: moviment.durada ? timeDuration(moviment.durada) : "Sense durada",
+        avatar: (
+          <Typography.Text type="secondary" style={{ fontSize: "small" }}>
+            {moviment.ordre}
+          </Typography.Text>
+        ),
       }))}
       action={<ModalAfegirMoviment getMoviments={getMoviments} />}
+      extra={durada_total && `Total: ${timeDuration(durada_total)}`}
     />
   );
 };
