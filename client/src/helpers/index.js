196d0f588ea141e87de21a8bdcf9df729308dd03
albert <albertmasa2@gmail.com>
2020-06-20 00:39:03 +0200

    feat: Implementa usePostAPI

--- client/src/helpers/index.js
+++ client/src/helpers/index.js
@@ -2,3 +2,4 @@ export { default as fetchAPI } from "./fetch-api";
 export { default as joinComponents } from "./join-components";
 export { default as showDeleteConfirm } from "./show-delete-confirm";
 export { default as useAPI } from "./use-api";
+export { default as usePostAPI } from "./use-post-api";
---
+++ client/src/helpers/use-post-api.js
@@ -0,0 +1,21 @@
+import { useCallback, useState } from "react";
+import { useDispatch } from "react-redux";
+import { fetchAPI } from "./index";
+
+export default (url) => {
+  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(false);
+
+  const postData = useCallback(
+    (data) => {
+      setLoading(true);
+      return fetchAPI(url, () => setLoading(false), dispatch, {
+        method: "POST",
+        body: JSON.stringify(data),
+      });
+    },
+    [url, dispatch]
+  );
+
+  return [loading, postData];
+};
