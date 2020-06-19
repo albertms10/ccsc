07d3d731c3e089af0ae3c476bdb13efc11a9bc9c
albert <albertmasa2@gmail.com>
2020-06-20 00:40:02 +0200

    fix: Corregeix valors de títol de moviments

--- server/queries/assajos/select__moviments_assaig.sql
+++ server/queries/assajos/select__moviments_assaig.sql
@@ -1,5 +1,7 @@
-SELECT *
+SELECT *,
+       m.titol AS titol_moviment,
+       o.titol AS titol_obra
 FROM moviments_esdeveniment_musical
-         INNER JOIN moviments USING (id_moviment)
-         INNER JOIN obres USING (id_obra)
+         INNER JOIN moviments m USING (id_moviment)
+         INNER JOIN obres o USING (id_obra)
 WHERE id_esdeveniment_musical = ?;
