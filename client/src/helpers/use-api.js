9bc22c83a690cbfd2f6ace1fc7484ab6659d9175
albert <albertmasa2@gmail.com>
2020-06-20 00:39:19 +0200

    fix: Corregeix setLoading a useAPI

--- client/src/helpers/use-api.js
+++ client/src/helpers/use-api.js
@@ -7,21 +7,19 @@ export default (url, initialState = []) => {
   const [data, setData] = useState(initialState);
   const [loading, setLoading] = useState(false);
 
-  const getData = useCallback(
-    () =>
-      fetchAPI(
-        url,
-        (data) => {
-          setData(data);
-          setLoading(false);
-        },
-        dispatch
-      ),
-    [url, dispatch]
-  );
+  const getData = useCallback(() => {
+    setLoading(true);
+    return fetchAPI(
+      url,
+      (data) => {
+        setData(data);
+        setLoading(false);
+      },
+      dispatch
+    );
+  }, [url, dispatch]);
 
   useEffect(() => {
-    setLoading(true);
     getData();
   }, [getData]);
 
