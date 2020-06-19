06673402866c11bdde1225d09c76dc248d20d852
albert <albertmasa2@gmail.com>
2020-06-19 14:11:40 +0200

    fix: Torna a incloure react-sticky com a component de Formacio

--- client/src/pages-tauler/formacio/formacio.js
+++ client/src/pages-tauler/formacio/formacio.js
@@ -1,12 +1,13 @@
-import { Affix, Tabs } from "antd";
+import { Tabs } from "antd";
 import React, { createContext, useContext, useEffect } from "react";
+import { Sticky, StickyContainer } from "react-sticky";
 import { IconFormacio } from "../../assets/icons";
 import { CalendariFormacio } from "../../components/calendari-formacio";
 import { SetPageHeaderContext } from "../../components/tauler-app/components/site-layout/site-layout";
 import { ContentHeader } from "../../standalone/content-header";
 import { FormacioPropTypes } from "../../typedef/prop-types";
-import "./formacio.css";
 import { ResumFormacio } from "./components/resum-formacio";
+import "./formacio.css";
 
 export const FormacioContext = createContext({});
 
@@ -24,28 +25,28 @@ const Formacio = ({ formacio }) => {
         subtitle={formacio.descripcio}
         icon={<IconFormacio name={formacio.nom_curt} />}
         footer={
-          <div className="tabs-formacio">
-            <Tabs
-              renderTabBar={(props, DefaultTabBar) => (
-                <Affix offsetTop={64}>
-                  <DefaultTabBar {...props} />
-                </Affix>
-              )}
-            >
-              <TabPane tab="Resum" key="resum">
-                <ResumFormacio />
-              </TabPane>
-              <TabPane
-                tab="Calendari"
-                key="calendar"
-                style={{ backgroundColor: "var(--background-color)" }}
+          <StickyContainer>
+            <div className="tabs-formacio">
+              <Tabs
+                renderTabBar={(props, DefaultTabBar) => (
+                  <Sticky bottomOffset={64}>
+                    {({ style }) => <DefaultTabBar {...props} style={style} />}
+                  </Sticky>
+                )}
               >
-                <CalendariFormacio />
-              </TabPane>
-              <TabPane tab="Projectes" key="projectes" />
-              <TabPane tab="Integrants" key="integrants" />
-            </Tabs>
-          </div>
+                <TabPane tab="Resum" key="resum">
+                  <ResumFormacio />
+                </TabPane>
+                <TabPane
+                  tab="Calendari"
+                  key="calendar"
+                  style={{ backgroundColor: "var(--background-color)" }}
+                >
+                  <CalendariFormacio />
+                </TabPane>
+              </Tabs>
+            </div>
+          </StickyContainer>
         }
       />
     </FormacioContext.Provider>
