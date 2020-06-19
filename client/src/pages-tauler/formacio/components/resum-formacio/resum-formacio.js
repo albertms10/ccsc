a7bffd1bf417d20d1d0225f918465bee654345bf
albert <albertmasa2@gmail.com>
2020-06-19 14:11:56 +0200

    feat: Canvia la disposició de les cards a ResumFormacio

--- client/src/pages-tauler/formacio/components/resum-formacio/resum-formacio.js
+++ client/src/pages-tauler/formacio/components/resum-formacio/resum-formacio.js
@@ -1,4 +1,4 @@
-import { Col, Row } from "antd";
+import { Col, Row, Typography } from "antd";
 import React, { useContext } from "react";
 import { ColorCardList } from "../../../../standalone/color-card-list";
 import { Container } from "../../../../standalone/container";
@@ -18,44 +18,57 @@ export default () => {
   const [persones, loadingIntegrants] = useIntegrants(id_formacio);
 
   return (
-    <Container>
-      <ColorCardList
-        dataSource={projectes}
-        loading={loadingProjectes}
-        mapItem={({ id_projecte, titol, directors, formacions, color }) => {
-          const altresFormacions = formacions.filter(
-            (formacio) => formacio.id_formacio !== id_formacio
-          );
+    <>
+      <Container>
+        <Typography.Title
+          level={4}
+          style={{ marginBottom: ".75rem", color: "#555" }}
+        >
+          Projectes
+        </Typography.Title>
+        <ColorCardList
+          dataSource={projectes}
+          loading={loadingProjectes}
+          mapItem={({ id_projecte, titol, directors, formacions, color }) => {
+            const altresFormacions = formacions.filter(
+              (formacio) => formacio.id_formacio !== id_formacio
+            );
 
-          return {
-            title: titol,
-            color: "#" + color,
-            description:
-              directors.length > 0
-                ? `Amb ${literalList(directors.map(({ nom }) => nom))}`
-                : altresFormacions.length > 0
-                ? `Amb ${literalList(
-                    altresFormacions.map(({ nom_curt }) => nom_curt)
-                  )}`
-                : "",
-            link: `/projectes/${id_projecte}`,
-          };
-        }}
-        style={{ marginBottom: 32 }}
-      />
-      <Row type="flex" gutter={[32, 32]}>
-        <Col sm={24} lg={12} flex={1}>
-          <ContentListAssajos assajos={assajos} loading={loadingAssajos} />
-          <ContentListConcerts concerts={concerts} loading={loadingConcerts} />
-        </Col>
-        <Col sm={24} lg={12} flex={1}>
-          <ContentListPersones
-            title="Integrants"
-            persones={persones}
-            loading={loadingIntegrants}
-          />
-        </Col>
-      </Row>
-    </Container>
+            return {
+              title: titol,
+              color: "#" + color,
+              description:
+                directors.length > 0
+                  ? `Amb ${literalList(directors.map(({ nom }) => nom))}`
+                  : altresFormacions.length > 0
+                  ? `Amb ${literalList(
+                      altresFormacions.map(({ nom_curt }) => nom_curt)
+                    )}`
+                  : "",
+              link: `/projectes/${id_projecte}`,
+            };
+          }}
+          style={{ marginBottom: 0 }}
+        />
+      </Container>
+      <Container>
+        <Row type="flex" gutter={[32, 32]}>
+          <Col sm={24} lg={12} flex={1}>
+            <ContentListAssajos assajos={assajos} loading={loadingAssajos} />
+            <ContentListConcerts
+              concerts={concerts}
+              loading={loadingConcerts}
+            />
+          </Col>
+          <Col sm={24} lg={12} flex={1}>
+            <ContentListPersones
+              title="Integrants"
+              persones={persones}
+              loading={loadingIntegrants}
+            />
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 };
