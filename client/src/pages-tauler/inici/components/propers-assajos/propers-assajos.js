55fb8d1de680bf90949f56fd39f68a213b8459eb
albert <albertmasa2@gmail.com>
2020-06-22 00:26:36 +0200

    feat: Canvia l’enllaç de la Card PropersAssajos per més detalls

--- client/src/pages-tauler/inici/components/propers-assajos/propers-assajos.js
+++ client/src/pages-tauler/inici/components/propers-assajos/propers-assajos.js
@@ -1,4 +1,8 @@
-import { BookOutlined, LayoutOutlined } from "@ant-design/icons";
+import {
+  BookOutlined,
+  InfoCircleOutlined,
+  LayoutOutlined,
+} from "@ant-design/icons";
 import { Card, Space, Spin, Tag, Typography } from "antd";
 import moment from "moment";
 import React from "react";
@@ -20,64 +24,65 @@ export default ({ style }) => {
     <div style={{ margin: "0 -1rem", ...style }}>
       <Spin spinning={loadingProperAssaig}>
         <div key={properAssaig.id_assaig} className="propers-assajos-wrapper">
-          <Link to={`/assajos/${properAssaig.id_assaig}`}>
-            <Card
-              hoverable
-              title={
-                <Space>
-                  <CalendarAvatar moment={moment(properAssaig.data_inici)} />
-                  {properAssaig.titol}
-                  <StatusIcon
-                    tooltip={properAssaig.estat_esdeveniment}
-                    statusId={properAssaig.id_estat_esdeveniment}
-                  />
-                  <Typography.Text
-                    type="secondary"
-                    style={{ fontWeight: "normal" }}
-                  >
-                    {timeRange(
-                      properAssaig.hora_inici,
-                      properAssaig.hora_final
-                    )}
-                  </Typography.Text>
-                </Space>
-              }
-            >
-              <Space direction="vertical">
-                <EventLineItem>
-                  {joinComponents(
-                    dateRange(
-                      properAssaig.dia_inici,
-                      properAssaig.hora_inici,
-                      properAssaig.dia_final,
-                      properAssaig.hora_final
-                    ),
-                    (item, index) => (
-                      <span key={index}>{item}</span>
-                    ),
-                    (key) => (
-                      <span key={key} style={{ padding: "0 .25rem" }}>
-                        ·
-                      </span>
-                    )
-                  )}
-                </EventLineItem>
-                <EventLineItem icon={<LayoutOutlined />}>
-                  <Space>
-                    <FixedTagsProjectes projectes={properAssaig.projectes} />
-                    <IconsFormacions formacions={properAssaig.formacions} />
-                  </Space>
-                </EventLineItem>
-                {properAssaig.moviments && properAssaig.moviments.length > 0 && (
-                  <EventLineItem icon={<BookOutlined />}>
-                    {properAssaig.moviments.map((moviment) => (
-                      <Tag>{moviment.titol_moviment}</Tag>
-                    ))}
-                  </EventLineItem>
-                )}
+          <Card
+            title={
+              <Space>
+                <CalendarAvatar moment={moment(properAssaig.data_inici)} />
+                {properAssaig.titol}
+                <StatusIcon
+                  tooltip={properAssaig.estat_esdeveniment}
+                  statusId={properAssaig.id_estat_esdeveniment}
+                />
+                <Typography.Text
+                  type="secondary"
+                  style={{ fontWeight: "normal" }}
+                >
+                  {timeRange(properAssaig.hora_inici, properAssaig.hora_final)}
+                </Typography.Text>
               </Space>
-            </Card>
-          </Link>
+            }
+          >
+            <Space direction="vertical">
+              <EventLineItem>
+                {joinComponents(
+                  dateRange(
+                    properAssaig.dia_inici,
+                    properAssaig.hora_inici,
+                    properAssaig.dia_final,
+                    properAssaig.hora_final
+                  ),
+                  (item, index) => (
+                    <span key={index}>{item}</span>
+                  ),
+                  (key) => (
+                    <span key={key} style={{ padding: "0 .25rem" }}>
+                      ·
+                    </span>
+                  )
+                )}
+              </EventLineItem>
+              <EventLineItem icon={<LayoutOutlined />}>
+                <Space>
+                  <FixedTagsProjectes projectes={properAssaig.projectes} />
+                  <IconsFormacions formacions={properAssaig.formacions} />
+                </Space>
+              </EventLineItem>
+              {properAssaig.moviments && properAssaig.moviments.length > 0 && (
+                <EventLineItem icon={<BookOutlined />}>
+                  {properAssaig.moviments.map((moviment) => (
+                    <Tag key={moviment.id_moviment}>
+                      {moviment.titol_moviment}
+                    </Tag>
+                  ))}
+                </EventLineItem>
+              )}
+              <EventLineItem icon={<InfoCircleOutlined />}>
+                <Link to={`/assajos/${properAssaig.id_assaig}`}>
+                  Més detalls
+                </Link>
+              </EventLineItem>
+            </Space>
+          </Card>
         </div>
       </Spin>
     </div>
