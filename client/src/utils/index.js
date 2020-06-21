c472b5a6540cacca47d5a5adaa27126d14903d1e
albert <albertmasa2@gmail.com>
2020-06-21 13:26:31 +0200

    feat: Afegeix funció útil mapFirstOfProperty

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -6,6 +6,7 @@ export { default as includesSome } from "./includes-some";
 export { default as initials } from "./initials";
 export { default as kebabCase } from "./kebab-case";
 export { default as literalList } from "./literal-list";
+export { default as mapFirstOfProperty } from "./map-first-of-property";
 export { default as nIndexOf } from "./n-index-of";
 export { default as pascalCase } from "./pascal-case";
 export { default as snakeCase } from "./snake-case";
---
+++ client/src/utils/map-first-of-property.js
@@ -0,0 +1,18 @@
+/**
+ * Returns a mapped array with a `first` boolean property
+ * that indicates if the given `property` has the first
+ * value occurrence.
+ * @param {[]} items
+ * @param {string} property
+ * @returns {[]}
+ */
+export default (items, property) => {
+  const itemsProperty = [];
+  return items.map((item) => ({
+    ...item,
+    first:
+      !itemsProperty.includes(items[property]) &&
+      itemsProperty.push(items[property]) &&
+      true,
+  }));
+};
