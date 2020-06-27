e05e64de876ae2201f6a2b5fe155035d4171fdd5
albert <albertmasa2@gmail.com>
2020-06-27 17:15:16 +0200

    fix: Elimina la key innecessària de PropersAssajos

--- client/src/pages-tauler/inici/components/propers-assajos/propers-assajos.js
+++ client/src/pages-tauler/inici/components/propers-assajos/propers-assajos.js
@@ -10,7 +10,7 @@ export default ({ style }) => {
   return (
     <div style={{ margin: "0 -1rem", ...style }}>
       <Spin spinning={loadingPropersAssajos}>
-        <div key={propersAssajos.id_assaig} className="propers-assajos-wrapper">
+        <div className="propers-assajos-wrapper">
           {propersAssajos.length > 0 ? (
             <CardProperAssaig assaig={propersAssajos[0]} />
           ) : (
