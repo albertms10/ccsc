3fe1b565834a626b274535b33768141ca5281509
albert <albertmasa2@gmail.com>
2020-06-28 14:35:49 +0200

    fix: Corregeix les JOINS a projectes_soci

--- server/queries/socis/select__projectes_soci.sql
+++ server/queries/socis/select__projectes_soci.sql
@@ -2,8 +2,8 @@ SET @id_soci = ?;
 
 SELECT DISTINCT pf.*
 FROM projectes_full pf
-         INNER JOIN projectes_formacions USING (id_projecte)
-         INNER JOIN socis_formacions USING (id_formacio)
+         LEFT JOIN projectes_formacions USING (id_projecte)
+         LEFT JOIN socis_formacions USING (id_formacio)
 WHERE id_soci = @id_soci
    OR EXISTS(
         SELECT *
