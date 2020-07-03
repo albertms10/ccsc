4772d1687f01e3c640b8058235f753a86a3d2222
albert <albertmasa2@gmail.com>
2020-07-03 16:55:33 +0200

    feat: Afegeix opció long a dateRange

--- client/src/utils/date-range.js
+++ client/src/utils/date-range.js
@@ -1,4 +1,5 @@
 import moment from "moment";
+import { nIndexOf } from "./index";
 
 /**
  * Returns the textual date and time range of two given dates
@@ -6,14 +7,29 @@ import moment from "moment";
  * @param {string} [timeStart]
  * @param {string} [dateEnd]
  * @param {string} [timeEnd]
+ * @param {Object} [options]
  * @returns {string[]}
  */
-export default (dateStart, timeStart, dateEnd, timeEnd) => [
-  moment(dateStart).format("dddd, LL"),
-  ...(timeStart
-    ? [
-        moment(`${dateStart} ${timeStart}`).format("LT") +
-          (timeEnd ? "–" + moment(`${dateEnd} ${timeEnd}`).format("LT") : ""),
-      ]
-    : []),
-];
+export default (
+  dateStart,
+  timeStart,
+  dateEnd,
+  timeEnd,
+  options = { long: true }
+) => {
+  const startFormat = moment(dateStart).format(
+    `${options.long ? "dddd, " : ""}LL`
+  );
+
+  return [
+    options.long
+      ? startFormat
+      : startFormat.substring(0, nIndexOf(startFormat, " de", 2)),
+    ...(timeStart
+      ? [
+          moment(`${dateStart} ${timeStart}`).format("LT") +
+            (timeEnd ? "–" + moment(`${dateEnd} ${timeEnd}`).format("LT") : ""),
+        ]
+      : []),
+  ];
+};
