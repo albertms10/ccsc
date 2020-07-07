d75ca9443881402f47cb0beb69373c0dbb1273d6
albert <albertmasa2@gmail.com>
2020-07-07 12:21:06 +0200

    fix: Explicita el nom del tipus_agrupacio a la consulta

--- server/queries/agrupacions/select__agrupacions.sql
+++ server/queries/agrupacions/select__agrupacions.sql
@@ -1,3 +1,4 @@
-SELECT *
-FROM agrupacions
-         INNER JOIN tipus_agrupacions USING (id_tipus_agrupacio);
+SELECT a.*,
+       ta.nom AS tipus
+FROM agrupacions a
+         INNER JOIN tipus_agrupacions ta USING (id_tipus_agrupacio);
