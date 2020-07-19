bc6bd3f117646fb4044ee6add3a694a9c0ef14dc
albert <albertmasa2@gmail.com>
2020-07-20 01:27:51 +0200

    feat: Afegeix server LogRocket al client

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,9 +1,12 @@
 import "ant-design-pro/dist/ant-design-pro.css";
+import LogRocket from "logrocket";
 import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./components/app";
 import "./index.css";
 import * as serviceWorker from "./service-worker";
+
+LogRocket.init("kyrqbw/albert-manosa");
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
