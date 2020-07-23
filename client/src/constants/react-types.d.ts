b2643fe5b7d0a314725df66508b03894ae40cffb
albert <albertmasa2@gmail.com>
2020-07-24 00:32:13 +0200

    feat: Afegeix mòdul react-types

---
+++ client/src/constants/react-types.d.ts
@@ -0,0 +1,6 @@
+declare module "react-types" {
+  export type useStatePair<T> = [
+    T,
+    import("react").Dispatch<import("react").SetStateAction<T>>
+  ];
+}
--- client/src/react-app-env.d.ts
+++ client/src/react-app-env.d.ts
@@ -1,3 +1,4 @@
 /// <reference types="react-scripts" />
+/// <reference path="./constants/react-types.d.ts" />
 /// <reference path="../../common/common.d.ts" />
 /// <reference path="../../common/model.d.ts" />
