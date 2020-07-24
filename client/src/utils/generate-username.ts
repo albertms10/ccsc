f06bf2fd2df4e5ea9c24640dd14ae3f3a77fa130
albert <albertmasa2@gmail.com>
2020-07-25 01:09:24 +0200

    fix: Prescindeix dels operadors de non-null assertion

--- client/src/utils/generate-username.ts
+++ client/src/utils/generate-username.ts
@@ -2,13 +2,13 @@ import { stripAccents } from "./index";
 
 export default (name: string, surname: string): string =>
   stripAccents(
-    name!
+    name
       .trim()
       .split(" ")
       .map((word, i) => (i > 0 ? word[0] : word))
       .join("")
       .toLowerCase() +
-      surname!
+      surname
         .trim()
         .split(" ")
         .map((word) => (word[0] === word[0].toUpperCase() ? word[0] : ""))
