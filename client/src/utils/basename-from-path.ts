505c0e9d1072df767103e6c8246fcfc2f11b776c
albert <albertmasa2@gmail.com>
2020-07-25 01:07:52 +0200

    fix: Corregeix la referència a pathname de basenameFromPath

--- client/src/utils/basename-from-path.ts
+++ client/src/utils/basename-from-path.ts
@@ -1,6 +1,6 @@
 import { nIndexOf } from "./index";
 
 export default (pathname: string, n: number): string => {
-  const subPathsIndex = nIndexOf(location.pathname, "/", n);
+  const subPathsIndex = nIndexOf(pathname, "/", n);
   return subPathsIndex !== -1 ? pathname.substr(0, subPathsIndex) : pathname;
 };
