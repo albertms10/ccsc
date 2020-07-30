eed821b952c1c8b513dbbc284d3fba112e1a1f3d
albert <albertmasa2@gmail.com>
2020-07-30 15:03:06 +0200

    refactor: Extreu usuaris_info en una VIEW

--- database/generation/database.views.sql
+++ database/generation/database.views.sql
@@ -3,6 +3,49 @@ SELECT id_usuari, username, creacio, id_persona
 FROM usuaris_complet;
 
 
+CREATE OR REPLACE VIEW usuaris_info AS
+SELECT id_usuari,
+       username,
+       nom,
+       cognoms,
+       es_dona,
+       id_persona,
+       salt,
+       encrypted_password,
+       (
+           SELECT JSON_ARRAYAGG(role)
+           FROM roles_usuaris
+                    INNER JOIN roles USING (id_role)
+           WHERE id_usuari = uc.id_usuari
+       )     AS roles,
+       (
+           SELECT IFNULL(JSON_ARRAYAGG(unique_name), '[]')
+           FROM avisos
+                    INNER JOIN tipus_avisos USING (id_tipus_avis)
+                    INNER JOIN acceptacions_avis av USING (id_avis)
+           WHERE requerida IS TRUE
+             AND NOT EXISTS(
+                   SELECT *
+                   FROM socis_acceptacions
+                   WHERE id_acceptacio_avis = av.id_acceptacio_avis
+                     AND accepta IS TRUE
+                     AND id_soci = p.id_persona
+               )
+       )     AS avisos,
+       EXISTS(
+               SELECT *
+               FROM socis s
+                        INNER JOIN historial_socis hs ON (s.id_soci = hs.id_historial_soci)
+               WHERE id_soci = p.id_persona
+                 AND CURRENT_DATE
+                   BETWEEN data_alta
+                   AND IFNULL(DATE_SUB(data_baixa, INTERVAL 1 DAY), CURRENT_DATE)
+               ORDER BY data_alta DESC
+           ) AS es_actiu
+FROM usuaris_complet uc
+         LEFT JOIN persones p USING (id_persona);
+
+
 CREATE OR REPLACE VIEW esdeveniments_estat AS
 SELECT DISTINCT id_esdeveniment,
                 IFNULL(CONCAT(dia_inici, ' ', hora_inici), dia_inici)                    AS data_inici,
