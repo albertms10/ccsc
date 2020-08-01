f95524efa6e6fa8a2b6faed7089f76e2857d9a89
albert <albertmasa2@gmail.com>
2020-08-01 23:26:19 +0200

    feat: Millora el tipat de parseAndSendJSON

--- server/src/helpers/parse-and-send-json.ts
+++ server/src/helpers/parse-and-send-json.ts
@@ -8,15 +8,17 @@ const parseArrayJSON = <T>(properties: (keyof T)[], element: T) => {
 
 const typeOf = <T>(data: T): string => Object.prototype.toString.call(data);
 
+type ArrayElement<T> = T extends Array<infer U> ? U : T;
+
 /**
  * Parses given properties of a data object
  * or array and sends a JSON response.
  */
-export default <T extends [] | {} | string>(
-  res: Response,
+export default <T, R extends string | Object | Array<any>>(
+  res: Response<T>,
   next: NextFunction,
-  data: T,
-  properties: (keyof T)[] = []
+  data: R,
+  properties: (keyof (R extends Array<any> ? ArrayElement<R> : R))[] = []
 ) => {
   if (!data) return;
 
@@ -24,13 +26,16 @@ export default <T extends [] | {} | string>(
     const dataType = typeOf(data);
 
     if (dataType === "[object Array]")
-      (data as []).forEach((element) => parseArrayJSON(properties, element));
-    else if (dataType === "[object Object]") parseArrayJSON(properties, data);
+      (data as Array<any>).forEach((element) =>
+        parseArrayJSON(properties, element)
+      );
+    else if (dataType === "[object Object]")
+      parseArrayJSON(properties, data as any);
     else if (dataType === "[object String]") data = JSON.parse(data as string);
   } catch (e) {
     next(e);
     return res.end();
   }
 
-  res.json(data);
+  res.json(data as any);
 };
