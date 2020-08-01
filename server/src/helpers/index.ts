346386b94f4a24873c7a0eecd0a76a96b1a823d4
albert <albertmasa2@gmail.com>
2020-08-01 17:03:01 +0200

    feat: Millora el tipat de parseAndSendJSON

--- server/src/helpers/index.ts
+++ server/src/helpers/index.ts
@@ -1,3 +1,3 @@
-export { default as trySendUser } from "./try-send-user";
 export { default as parseAndSendJSON } from "./parse-and-send-json";
 export { default as queryFile } from "./query-file";
+export { default as trySendUser } from "./try-send-user";
--- server/src/helpers/parse-and-send-json.ts
+++ server/src/helpers/parse-and-send-json.ts
@@ -1,30 +1,32 @@
 import { NextFunction, Response } from "express";
 
+const parseArrayJSON = <T>(properties: (keyof T)[], element: T) => {
+  properties.forEach(
+    (property) => (element[property] = JSON.parse(element[property as string]))
+  );
+};
+
+const typeOf = <T>(data: T): string => Object.prototype.toString.call(data);
+
 /**
  * Parses given properties of a data object
  * or array and sends a JSON response.
  */
-export default (
+export default <T extends [] | {} | string>(
   res: Response,
   next: NextFunction,
-  data: any,
-  properties: string[] = []
+  data: T,
+  properties: (keyof T)[] = []
 ) => {
   if (!data) return;
 
-  const parseArrayJSON = (properties: string[], element: Object) =>
-    properties.forEach(
-      (property) => (element[property] = JSON.parse(element[property]))
-    );
-
-  const typeOf = (data: any) => Object.prototype.toString.call(data);
-
   try {
-    if (typeOf(data) === "[object Array]")
-      data.forEach((element) => parseArrayJSON(properties, element));
-    else if (typeOf(data) === "[object Object]")
-      parseArrayJSON(properties, data);
-    else if (typeOf(data) === "[object String]") data = JSON.parse(data);
+    const dataType = typeOf(data);
+
+    if (dataType === "[object Array]")
+      (data as []).forEach((element) => parseArrayJSON(properties, element));
+    else if (dataType === "[object Object]") parseArrayJSON(properties, data);
+    else if (dataType === "[object String]") data = JSON.parse(data as string);
   } catch (e) {
     next(e);
     return res.end();
