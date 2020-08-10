08333531754e8c1ff891c383cf75ff6fa55d6748
albert <albertmasa2@gmail.com>
2020-08-10 19:43:17 +0200

    fix: Corregeix el cast a JSON

--- server/src/queries/socis/select__acceptacions_soci.sql
+++ server/src/queries/socis/select__acceptacions_soci.sql
@@ -1,7 +1,7 @@
-SELECT IFNULL(
-               JSON_OBJECTAGG(form_name, IF(accepta, CAST(TRUE AS JSON), CAST(FALSE AS JSON))),
-               '{}'
-           ) AS acceptacions
+SELECT CAST(IFNULL(
+        JSON_OBJECTAGG(form_name, IF(accepta, CAST(TRUE AS JSON), CAST(FALSE AS JSON))),
+        '{}'
+    ) AS JSON) AS acceptacions
 FROM socis_acceptacions
          INNER JOIN acceptacions_avis USING (id_acceptacio_avis)
 WHERE id_soci = ?;
