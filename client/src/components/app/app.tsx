776375c7cffb3f80c2a8135383be5fcc317644a4
albert <albertmasa2@gmail.com>
2020-08-21 01:12:38 +0200

    perf: Carrega amb lazy els components Home i TaulerApp

--- client/src/components/app/app.tsx
+++ client/src/components/app/app.tsx
@@ -1,20 +1,27 @@
 import { ConfigProvider } from "antd";
 import caES from "antd/es/locale/ca_ES";
-import { HomeApp } from "components/home-app";
-import { TaulerApp } from "components/tauler-app";
 import moment from "moment";
 import "moment/locale/ca";
 import { IniciaSessio } from "pages-home/inicia-sessio";
 import { AvisosInici } from "pages-home/inicia-sessio/components/avisos-inici";
 import { DonarAlta } from "pages-home/inicia-sessio/components/donar-alta";
 import { DonarAltaFormulari } from "pages-home/inicia-sessio/components/donar-alta-formulari";
-import React from "react";
+import React, { lazy } from "react";
 import { useTranslation } from "react-i18next";
 import { Provider as StoreProvider } from "react-redux";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { store } from "store";
 import { linkText } from "utils";
 import { RouteTauler } from "./components/route-tauler";
+
+const HomeApp = lazy(() =>
+  import("components/home-app/home-app" /* webpackChunkName: "home-app" */)
+);
+const TaulerApp = lazy(() =>
+  import(
+    "components/tauler-app/tauler-app" /* webpackChunkName: "tauler-app" */
+  )
+);
 
 moment.locale("ca");
 
