ac6162f1e0c0ca1206ad18d76f6b048769c17911
albert <albertmasa2@gmail.com>
2020-08-27 13:35:52 +0200

    feat: Afegeix informació de la url amb Breadcrumb a UnknownPage

--- client/src/standalone/unknown-page/unknown-page.tsx
+++ client/src/standalone/unknown-page/unknown-page.tsx
@@ -1,15 +1,25 @@
-import { Typography } from "antd";
+import { Breadcrumb, Typography } from "antd";
 import React from "react";
 import { useTranslation } from "react-i18next";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { prettyString, upperCaseFirst } from "utils";
 import "./unknown-page.css";
 
 const UnknownPage: React.FC = () => {
   const { t } = useTranslation("dashboard");
 
+  const location = useLocation();
+
   return (
     <div className="unknown-page-container">
       <Typography.Title level={2}>{t("unknown page")}</Typography.Title>
+      <Breadcrumb separator="›" style={{ marginBottom: 16 }}>
+        {location.pathname.split("/").map((path, index) => (
+          <Breadcrumb.Item key={`${path}-${index}`}>
+            {upperCaseFirst(prettyString(path))}
+          </Breadcrumb.Item>
+        ))}
+      </Breadcrumb>
       <Link to="/">{t("go back home")}</Link>
     </div>
   );
