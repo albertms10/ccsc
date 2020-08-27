21b998643984e7d61801d6e3234af227e034fdda
albert <albertmasa2@gmail.com>
2020-08-27 12:31:06 +0200

    fix: Corregeix l’ordre de definició de les routes d’entitats

--- server/src/routes/entitats.routes.ts
+++ server/src/routes/entitats.routes.ts
@@ -6,12 +6,12 @@ const router = express.Router();
 
 router.route("/").get([verifyAccessToken], controller.entitats_get);
 
-router.route("/:id").get([verifyAccessToken], controller.entitats_detall);
-
-router.route("/avisos/:name").get(controller.entitats_detall_avisos_detall);
-
 router
   .route("/cursos")
   .get([verifyAccessToken], controller.entitats_detall_cursos);
+
+router.route("/avisos/:name").get(controller.entitats_detall_avisos_detall);
+
+router.route("/:id").get([verifyAccessToken], controller.entitats_detall);
 
 export default router;
