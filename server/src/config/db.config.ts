5854009be84511697cd71d679ddde01599d15f84
albertms10 <albertmasa2@gmail.com>
2020-12-23 01:08:33 +0100

    refactor(db.config): extract booleanFields for better code readability

--- server/src/config/db.config.ts
+++ server/src/config/db.config.ts
@@ -3,6 +3,27 @@ import * as mysql from "mysql2/promise";
 
 dotenv.config();
 
+const booleanFields = [
+  "amb_retard",
+  "convocada",
+  "disponible",
+  "dins_periode",
+  "email_exists",
+  "es_actiu",
+  "es_anonim",
+  "es_assaig_ordinari",
+  "es_dona",
+  "es_extra",
+  "es_general",
+  "es_institucio",
+  "es_parcial",
+  "es_role",
+  "es_unic_moviment",
+  "requerida",
+  "requerit",
+  "treballat",
+];
+
 export default mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -12,27 +33,11 @@ export default mysql.createPool({
   multipleStatements: true,
   timezone: "Z",
   dateStrings: true,
-  typeCast: (field, next) =>
-    [
-      "amb_retard",
-      "convocada",
-      "disponible",
-      "dins_periode",
-      "email_exists",
-      "es_actiu",
-      "es_anonim",
-      "es_assaig_ordinari",
-      "es_dona",
-      "es_extra",
-      "es_general",
-      "es_institucio",
-      "es_parcial",
-      "es_role",
-      "es_unic_moviment",
-      "requerida",
-      "requerit",
-      "treballat",
-    ].includes(field.name)
-      ? field.string() === "1"
-      : next(),
+  typeCast: (field, next) => {
+    if (booleanFields.includes(field.name)) {
+      return field.string() === "1";
+    }
+
+    return next();
+  },
 });
